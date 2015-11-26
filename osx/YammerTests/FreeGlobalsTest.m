2b5bec8e6fe27f16a859620bc05815293138b866
gygias <gygias@users.noreply.github.com>
2015-11-25 21:38:00 -0800

    .

--- osx/YammerTests/FreeGlobalsTest.m
+++ osx/YammerTests/FreeGlobalsTest.m
@@ -29,11 +29,4 @@
     CFRunLoopRunInMode(kCFRunLoopDefaultMode, 2, false);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
 @end
