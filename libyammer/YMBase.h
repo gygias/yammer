bd6f79d04a47e5ba22cba48d861e442c80a36f0a
gygias <gygias@users.noreply.github.com>
2015-11-30 04:21:42 -0800

    clean up win32 build

--- libyammer/YMBase.h
+++ libyammer/YMBase.h
@@ -22,11 +22,17 @@ extern "C" {
 #endif
 
 #ifdef WIN32
-#define YMAPI __declspec( dllimport ) 
+# ifndef YMAPI
+# define YMAPI __declspec( dllimport )
+# endif
+#define YMFILE HANDLE
+#define YMSOCKET SOCKET
 #define __printflike(x,y)
 #define YM_VARGS_SENTINEL_REQUIRED
 #else
 #define YMAPI
+#define YMFILE int
+#define YMSOCKET YMFILE
 #endif
 
 typedef const void *YMTypeRef;
@@ -43,55 +49,6 @@ YMAPI YM_RELEASE_RETURN_TYPE YMRelease(YMTypeRef object);
 
 YMAPI void YMSelfLock(YMTypeRef object);
 YMAPI void YMSelfUnlock(YMTypeRef object);
-
-#ifndef WIN32
-    #define YMFILE int
-    #define NULL_FILE (-1)
-    #define YM_READ_FILE(fd,addr,count) aRead = read(fd, addr, count);
-    #define YM_WRITE_FILE(fd,addr,count) aWrite = write(fd,addr,count);
-    #define YM_CLOSE_FILE(fd) { result = close(fd); \
-                                if ( result != 0 ) { error = errno; errorStr = strerror(error); } \
-                                else { error = 0; errorStr = NULL; } }
-    #define YMSOCKET YMFILE
-    #define NULL_SOCKET (-1)
-    #define YM_CLOSE_SOCKET(socket) YM_CLOSE_FILE(socket)
-    #define YM_WAIT_SEMAPHORE(s) { result = sem_wait(s->sem); \
-                                    if ( result != 0 ) { error = errno; errorStr = strerror(error); } \
-                                    else { error = 0; errorStr = NULL; } }
-    #define YM_POST_SEMAPHORE(s) { result = sem_post(semaphore->sem); \
-                                    if ( result != 0 ) { error = errno; errorStr = strerror(error); } \
-                                    else { error = 0; errorStr = NULL; } }
-    #define YM_RETRY_SEMAPHORE ( error == EINTR )
-    #define YM_CLOSE_SEMAPHORE(s) { result = sem_unlink(YMSTR(s->semName)); \
-                                    if ( result != 0 ) { error = errno; errorStr = strerror(error); } \
-                                    else { error = 0; errorStr = NULL; } }
-#else
-    #define YMFILE HANDLE
-    #define NULL_FILE ((HANDLE)NULL)
-    #define YM_READ_FILE(fd,addr,count) { BOOL okay = ReadFile(fd, buffer + off, bytes - off, &aRead, NULL); if ( ! okay ) aRead = -1; }
-    #define YM_WRITE_FILE(fd,addr,count) { BOOL okay = WriteFile(fd, buffer+ off, bytes - off, &aWrite, NULL); if ( ! okay ) aWrite = -1; }
-    #define YM_CLOSE_FILE(fd) { BOOL okay = CloseHandle(fd); if ( ! okay ) result = -1; else result = 0; }
-    #define YMSOCKET SOCKET
-    #define NULL_SOCKET ((SOCKET)NULL)
-	#define GENERIC_WERROR_STR "windows error" // unfortunately the strerror equivalent FormatMessage needs the caller to take ownership, which we don't want to mess with.
-												// we could have our own method with static strings. P.S. ERROR_ARENA_TRASHED
-    #define YM_CLOSE_SOCKET(socket) { result = closesocket(socket); \
-                                      if ( result == SOCKET_ERROR ) { result = -1; error = GetLastError(); errorStr = GENERIC_WERROR_STR; } \
-                                      else { result = 0; error = 0; errorStr = NULL; } }
-    #define YM_WAIT_SEMAPHORE(s) { DWORD dResult = WaitForSingleObject(s->sem, INFINITE); \
-                                   if ( dResult != WAIT_OBJECT_0 ) { \
-                                        if ( dResult == WAIT_FAILED ) error = (int)GetLastError(); \
-                                        else error = (int)dResult; \
-                                        result = -1; errorStr = GENERIC_WERROR_STR; } \
-                                    else { result = 0; error = 0; errorStr = NULL; } }
-    #define YM_POST_SEMAPHORE(s) { BOOL okay = ReleaseSemaphore(s->sem, 1, NULL); \
-                                    if ( ! okay ) { result = -1; error = GetLastError(); errorStr = GENERIC_WERROR_STR; } \
-                                    else { result = 0; error = 0; errorStr = NULL; } }
-    #define YM_RETRY_SEMAPHORE false
-    #define YM_CLOSE_SEMAPHORE(s) { BOOL okay = CloseHandle(s->sem); \
-                                    if ( ! okay ) { result = -1; error = (int)GetLastError(); errorStr = GENERIC_WERROR_STR; } \
-                                    else { result = 0; error = 0; errorStr = NULL; } }
-#endif
 
 typedef enum
 {
