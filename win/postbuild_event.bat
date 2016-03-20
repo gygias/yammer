58bd245c7d53f905d58e23f5b3b4c075824b136d
gygias <gygias@users.noreply.github.com>
2016-03-19 23:36:10 -0700

    torture postbuild_event into working for other directories, and add it to side projects

--- win/postbuild_event.bat
+++ win/postbuild_event.bat
@@ -1,8 +1,33 @@
-xcopy /Y lib\* Debug\
-IF NOT %ERRORLEVEL% EQU 2 IF NOT %ERRORLEVEL% EQU 0 (
-  ECHO xcopy returned %ERRORLEVEL%
-  exit /B 1
+cd
+
+:: are VS macros really unavailable from 'build events'?
+set DEBUG=Debug
+set RELEASE=Release
+set CPSRC=lib\*
+IF NOT EXIST lib (
+	set CPSRC=..\%CPSRC%
 )
 
-ECHO copied lib\* to Debug
+set CPDST=%DEBUG%
+
+:CPAGAIN
+xcopy /Y %CPSRC% %CPDST%\
+IF NOT %ERRORLEVEL% EQU 2 IF NOT %ERRORLEVEL% EQU 0 (
+	IF NOT EXIST %CPDST% (
+		set CPDST=%RELEASE%
+		GOTO CPAGAIN
+	)
+	ECHO xcopy %CPSRC% %CPDST% returned %ERRORLEVEL%
+	exit /B 1
+)
+
+ECHO copied %CPSRC% to %CPDST%
+
+IF EXIST %RELEASE% (
+	IF %CPDST% neq %RELEASE% (
+		set CPDST=%RELEASE%
+		GOTO CPAGAIN
+	)
+)
+
 exit /B 0
