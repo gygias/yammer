d64bc0fac24da862f37742006cdb2fe490dc7547
gygias <gygias@users.noreply.github.com>
2016-03-27 11:13:10 -0700

    add SIGCHLD to lldb source script

--- osx/YammerTests/lldb_source.sh
+++ osx/YammerTests/lldb_source.sh
@@ -1,6 +1,7 @@
 command script import lldb.macosx.heap
 
 command process handle -n true -p true -s false SIGPIPE
+command process handle -n true -p true -s false SIGCHLD
 
 command alias p print
 command alias mh malloc_info --stack-history
